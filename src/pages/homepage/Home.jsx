077cc38a7c7dfd18787773af47829c342bf137dc
Navi-th <naveen@rareminds.in>
2025-10-13 11:25:47 +0530

    Fix the mobile device responive

--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -4,7 +4,9 @@ const Home = () => {
   return (
     <div className="min-h-screen">
       <Hero />
-      <Features />
+      <div id="next-section">
+        <Features />
+      </div>
       <CoreFeatures />
       <BusinessImpact />
       <BuiltForCorporates />
