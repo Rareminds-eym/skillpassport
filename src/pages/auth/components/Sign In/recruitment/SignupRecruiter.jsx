19dbf2989093522cbb9df4e91b6b86da3305eb48
Sinjucodes <sinjpl41@gmail.com>
2025-10-31 17:13:51 +0530

    recruitment changes

--- src/pages/auth/components/Sign In/recruitment/SignupRecruiter.jsx
+++ src/pages/auth/components/Sign In/recruitment/SignupRecruiter.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import RecruiterSignupForm from './components/RecruiterSignupForm';
+import RecruiterSignupForm from '../RecruiterSignupForm';
 import { Link } from 'react-router-dom';
 
 const SignupRecruiter = () => {
