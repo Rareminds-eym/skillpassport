864cd6c546ddb1858fb76a4d1cc5b4a3bcff2eef
karthikeyans201219995 <karthikeyans201219995@gmail.com>
2025-12-09 21:49:46 +0530

    commited

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -344,6 +344,9 @@ const AttendanceReports = lazy(() =>
 const ClassManagement = lazy(() =>
   import("../pages/admin/schoolAdmin/ClassManagement")
 );
+const SchoolAdminAssessmentResults = lazy(() =>
+  import("../pages/admin/schoolAdmin/AssessmentResults")
+);
 
 // Settings
 const SchoolAdminSettings = lazy(() =>
@@ -511,6 +514,7 @@ const AppRoutes = () => {
           <Route path="dashboard" element={<SchoolAdminDashboard />} />
           <Route path="students/admissions" element={<StudentAdmissions />} />
           <Route path="students/attendance-reports" element={<AttendanceReports />} />
+          <Route path="students/assessment-results" element={<SchoolAdminAssessmentResults />} />
           <Route path="students/digital-portfolio" element={<SchoolAdminDigitalPortfolio />} />
           <Route path="classes/management" element={<ClassManagement />} />
           <Route path="teachers/list" element={<TeacherList />} />
